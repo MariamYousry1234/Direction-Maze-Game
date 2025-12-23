a88c03c8c0dbe8c07c9746bb2897553e0adcb155
MariamYousry1234 <mariamyousry030@gmail.com>
2025-12-23 13:03:40 +0200

    Add project files.

---
+++ Direction Maze Game.Designer.cs
@@ -0,0 +1,411 @@
+﻿
+namespace Direction_Maze_Game
+{
+    partial class DirectionMazeGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.panel15 = new System.Windows.Forms.Panel();
+            this.panel16 = new System.Windows.Forms.Panel();
+            this.panel17 = new System.Windows.Forms.Panel();
+            this.panel18 = new System.Windows.Forms.Panel();
+            this.panel19 = new System.Windows.Forms.Panel();
+            this.panel20 = new System.Windows.Forms.Panel();
+            this.panel21 = new System.Windows.Forms.Panel();
+            this.panel22 = new System.Windows.Forms.Panel();
+            this.pStart = new System.Windows.Forms.Panel();
+            this.pEnd = new System.Windows.Forms.Panel();
+            this.lblRounds = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblRoundsNumber = new System.Windows.Forms.Label();
+            this.lblScoreNumber = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRoundTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(48, 98);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(691, 35);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(48, 547);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(691, 32);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Location = new System.Drawing.Point(698, 130);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(41, 419);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Black;
+            this.panel4.Location = new System.Drawing.Point(48, 132);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(37, 417);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Black;
+            this.panel5.Location = new System.Drawing.Point(362, 133);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(38, 82);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Black;
+            this.panel6.Location = new System.Drawing.Point(85, 301);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(73, 32);
+            this.panel6.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Black;
+            this.panel7.Location = new System.Drawing.Point(611, 375);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(87, 34);
+            this.panel7.TabIndex = 6;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Black;
+            this.panel8.Location = new System.Drawing.Point(142, 183);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(172, 32);
+            this.panel8.TabIndex = 7;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Black;
+            this.panel9.Location = new System.Drawing.Point(221, 215);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(35, 188);
+            this.panel9.TabIndex = 8;
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.Color.Black;
+            this.panel10.Location = new System.Drawing.Point(256, 286);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(144, 32);
+            this.panel10.TabIndex = 9;
+            // 
+            // panel11
+            // 
+            this.panel11.BackColor = System.Drawing.Color.Black;
+            this.panel11.Location = new System.Drawing.Point(123, 396);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(133, 32);
+            this.panel11.TabIndex = 10;
+            // 
+            // panel12
+            // 
+            this.panel12.BackColor = System.Drawing.Color.Black;
+            this.panel12.Location = new System.Drawing.Point(123, 427);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(35, 68);
+            this.panel12.TabIndex = 11;
+            // 
+            // panel13
+            // 
+            this.panel13.BackColor = System.Drawing.Color.Black;
+            this.panel13.Location = new System.Drawing.Point(156, 463);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(173, 32);
+            this.panel13.TabIndex = 12;
+            // 
+            // panel14
+            // 
+            this.panel14.BackColor = System.Drawing.Color.Black;
+            this.panel14.Location = new System.Drawing.Point(294, 399);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(35, 64);
+            this.panel14.TabIndex = 13;
+            // 
+            // panel15
+            // 
+            this.panel15.BackColor = System.Drawing.Color.Black;
+            this.panel15.Location = new System.Drawing.Point(294, 371);
+            this.panel15.Name = "panel15";
+            this.panel15.Size = new System.Drawing.Size(247, 32);
+            this.panel15.TabIndex = 14;
+            // 
+            // panel16
+            // 
+            this.panel16.BackColor = System.Drawing.Color.Black;
+            this.panel16.Location = new System.Drawing.Point(506, 286);
+            this.panel16.Name = "panel16";
+            this.panel16.Size = new System.Drawing.Size(35, 87);
+            this.panel16.TabIndex = 15;
+            // 
+            // panel17
+            // 
+            this.panel17.BackColor = System.Drawing.Color.Black;
+            this.panel17.Location = new System.Drawing.Point(451, 286);
+            this.panel17.Name = "panel17";
+            this.panel17.Size = new System.Drawing.Size(55, 32);
+            this.panel17.TabIndex = 16;
+            // 
+            // panel18
+            // 
+            this.panel18.BackColor = System.Drawing.Color.Black;
+            this.panel18.Location = new System.Drawing.Point(451, 205);
+            this.panel18.Name = "panel18";
+            this.panel18.Size = new System.Drawing.Size(38, 82);
+            this.panel18.TabIndex = 17;
+            // 
+            // panel19
+            // 
+            this.panel19.BackColor = System.Drawing.Color.Black;
+            this.panel19.Location = new System.Drawing.Point(489, 205);
+            this.panel19.Name = "panel19";
+            this.panel19.Size = new System.Drawing.Size(144, 32);
+            this.panel19.TabIndex = 18;
+            // 
+            // panel20
+            // 
+            this.panel20.BackColor = System.Drawing.Color.Black;
+            this.panel20.Location = new System.Drawing.Point(598, 236);
+            this.panel20.Name = "panel20";
+            this.panel20.Size = new System.Drawing.Size(35, 87);
+            this.panel20.TabIndex = 19;
+            // 
+            // panel21
+            // 
+            this.panel21.BackColor = System.Drawing.Color.Black;
+            this.panel21.Location = new System.Drawing.Point(389, 463);
+            this.panel21.Name = "panel21";
+            this.panel21.Size = new System.Drawing.Size(35, 86);
+            this.panel21.TabIndex = 20;
+            // 
+            // panel22
+            // 
+            this.panel22.BackColor = System.Drawing.Color.Black;
+            this.panel22.Location = new System.Drawing.Point(423, 463);
+            this.panel22.Name = "panel22";
+            this.panel22.Size = new System.Drawing.Size(231, 32);
+            this.panel22.TabIndex = 21;
+            // 
+            // pStart
+            // 
+            this.pStart.BackColor = System.Drawing.Color.Blue;
+            this.pStart.Location = new System.Drawing.Point(91, 147);
+            this.pStart.Name = "pStart";
+            this.pStart.Size = new System.Drawing.Size(30, 30);
+            this.pStart.TabIndex = 22;
+            // 
+            // pEnd
+            // 
+            this.pEnd.BackColor = System.Drawing.Color.Lime;
+            this.pEnd.Location = new System.Drawing.Point(656, 511);
+            this.pEnd.Name = "pEnd";
+            this.pEnd.Size = new System.Drawing.Size(30, 30);
+            this.pEnd.TabIndex = 23;
+            // 
+            // lblRounds
+            // 
+            this.lblRounds.AutoSize = true;
+            this.lblRounds.Font = new System.Drawing.Font("Arial Black", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRounds.ForeColor = System.Drawing.Color.Red;
+            this.lblRounds.Location = new System.Drawing.Point(99, 617);
+            this.lblRounds.Name = "lblRounds";
+            this.lblRounds.Size = new System.Drawing.Size(145, 41);
+            this.lblRounds.TabIndex = 24;
+            this.lblRounds.Text = "Rounds:";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Arial Black", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.Color.Red;
+            this.lblScore.Location = new System.Drawing.Point(444, 617);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(119, 41);
+            this.lblScore.TabIndex = 25;
+            this.lblScore.Text = "Score:";
+            // 
+            // lblRoundsNumber
+            // 
+            this.lblRoundsNumber.AutoSize = true;
+            this.lblRoundsNumber.Font = new System.Drawing.Font("Arial Black", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRoundsNumber.ForeColor = System.Drawing.Color.Blue;
+            this.lblRoundsNumber.Location = new System.Drawing.Point(279, 617);
+            this.lblRoundsNumber.Name = "lblRoundsNumber";
+            this.lblRoundsNumber.Size = new System.Drawing.Size(37, 41);
+            this.lblRoundsNumber.TabIndex = 26;
+            this.lblRoundsNumber.Tag = "0";
+            this.lblRoundsNumber.Text = "0";
+            // 
+            // lblScoreNumber
+            // 
+            this.lblScoreNumber.AutoSize = true;
+            this.lblScoreNumber.Font = new System.Drawing.Font("Arial Black", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreNumber.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblScoreNumber.Location = new System.Drawing.Point(604, 617);
+            this.lblScoreNumber.Name = "lblScoreNumber";
+            this.lblScoreNumber.Size = new System.Drawing.Size(37, 41);
+            this.lblScoreNumber.TabIndex = 27;
+            this.lblScoreNumber.Tag = "0";
+            this.lblScoreNumber.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(111, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 41);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Round Time:";
+            // 
+            // lblRoundTime
+            // 
+            this.lblRoundTime.AutoSize = true;
+            this.lblRoundTime.Font = new System.Drawing.Font("Arial Black", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRoundTime.ForeColor = System.Drawing.Color.Blue;
+            this.lblRoundTime.Location = new System.Drawing.Point(382, 27);
+            this.lblRoundTime.Name = "lblRoundTime";
+            this.lblRoundTime.Size = new System.Drawing.Size(56, 41);
+            this.lblRoundTime.TabIndex = 29;
+            this.lblRoundTime.Tag = "30";
+            this.lblRoundTime.Text = "30";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // DirectionMazeGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 674);
+            this.Controls.Add(this.lblRoundTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblScoreNumber);
+            this.Controls.Add(this.lblRoundsNumber);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblRounds);
+            this.Controls.Add(this.pEnd);
+            this.Controls.Add(this.pStart);
+            this.Controls.Add(this.panel22);
+            this.Controls.Add(this.panel21);
+            this.Controls.Add(this.panel20);
+            this.Controls.Add(this.panel19);
+            this.Controls.Add(this.panel18);
+            this.Controls.Add(this.panel17);
+            this.Controls.Add(this.panel16);
+            this.Controls.Add(this.panel15);
+            this.Controls.Add(this.panel14);
+            this.Controls.Add(this.panel13);
+            this.Controls.Add(this.panel12);
+            this.Controls.Add(this.panel11);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "DirectionMazeGame";
+            this.Text = "Direction Maze Game";
+            this.Load += new System.EventHandler(this.DirectionMazeGame_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.DirectionMazeGame_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.Panel panel15;
+        private System.Windows.Forms.Panel panel16;
+        private System.Windows.Forms.Panel panel17;
+        private System.Windows.Forms.Panel panel18;
+        private System.Windows.Forms.Panel panel19;
+        private System.Windows.Forms.Panel panel20;
+        private System.Windows.Forms.Panel panel21;
+        private System.Windows.Forms.Panel panel22;
+        private System.Windows.Forms.Panel pStart;
+        private System.Windows.Forms.Panel pEnd;
+        private System.Windows.Forms.Label lblRounds;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblRoundsNumber;
+        private System.Windows.Forms.Label lblScoreNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRoundTime;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Direction Maze Game.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Direction_Maze_Game
+{
+    public partial class DirectionMazeGame : Form
+    {
+        public DirectionMazeGame()
+        {
+            InitializeComponent();
+        }
+
+    
+        List<Panel> FixedPanels = new List<Panel>();
+
+        void SaveEachFixedPanel()
+        {
+            FixedPanels.AddRange(new Panel[] { panel1,panel2, panel3, panel4,
+           panel5, panel6,panel7,panel8,panel9,panel10,panel11,
+           panel12,panel13, panel14,panel15, panel16, panel17, panel18,
+            panel19,panel20,panel21,panel22});
+        }
+
+        void IncreaseRoundNumber()
+        {
+            lblRoundsNumber.Tag = Convert.ToInt32(lblRoundsNumber.Tag) + 1;
+            lblRoundsNumber.Text = lblRoundsNumber.Tag.ToString();
+        }
+
+        void UpdateRoundTime()
+        {
+            lblRoundTime.Tag = Convert.ToSingle(lblRoundTime.Tag) - 1;
+            lblRoundTime.Text = lblRoundTime.Tag.ToString();
+
+            if (Convert.ToSingle(lblRoundTime.Tag) == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Time is up\nTry again");
+                IncreaseRoundNumber();
+                ResetGame();
+            }
+        }
+
+
+        bool IsPlayerHitWall()
+        {
+            foreach (Panel p in FixedPanels)
+            {
+                if (pStart.Bounds.IntersectsWith(p.Bounds))
+                    return true;
+            }
+
+            return false;
+        }
+
+        bool IsPlayerWin()
+        {
+            return pStart.Bounds.IntersectsWith(pEnd.Bounds);
+        }
+
+        void ResetGame()
+        {
+            pStart.Location = new Point(91, 147);
+            pStart.BackColor = Color.Blue;
+            lblRoundTime.Tag = 30;
+            lblRoundTime.Text = "30";
+            timer1.Start();
+        }
+
+
+        void EndRound()
+        {
+            if (IsPlayerHitWall())
+            {
+                timer1.Stop();
+                pStart.BackColor = Color.Red;
+                MessageBox.Show("You hit the wall\nTry again");
+                IncreaseRoundNumber();
+                ResetGame();
+                return;
+            }
+
+            if (IsPlayerWin())
+            {
+                timer1.Stop();
+
+                lblScoreNumber.Tag = Convert.ToInt32(lblScoreNumber.Tag) + 1;
+                MessageBox.Show($"Perfect! You win this round\nYour Score Number: {lblScoreNumber.Tag.ToString()}");
+                lblScoreNumber.Text = lblScoreNumber.Tag.ToString();
+                IncreaseRoundNumber();
+                ResetGame();
+            }
+        }
+
+
+        void Move(Keys key)
+        {
+            if (key == Keys.Up)
+            {
+                pStart.Top -= 10;
+            }
+
+            if (key == Keys.Down)
+            {
+                pStart.Top += 10;
+            }
+
+            if (key == Keys.Left)
+            {
+                pStart.Left -= 10;
+            }
+
+            if (key == Keys.Right)
+            {
+                pStart.Left += 10;
+            }
+            EndRound();
+        }
+
+
+        private void DirectionMazeGame_Load(object sender, EventArgs e)
+        {
+            SaveEachFixedPanel();
+            ResetGame();
+        }
+
+        private void DirectionMazeGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            Move(e.KeyData); 
+        }
+
+     
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateRoundTime();
+        }
+    }
+}
